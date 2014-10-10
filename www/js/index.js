b6f2fa8a59cb193c2305632e74f07791f7755d4e
Frank J. Gómez <frank@ginkgostreet.com>
2014-10-10 17:50:10 -0400

    Refactored navigation system to use more standard markup. General JS refactoring and reorganization. Removed some boilerplate cruft.

--- www/js/index.js
+++ www/js/index.js
@@ -26,50 +26,100 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
+        // wait for device API libraries to load
         document.addEventListener('deviceready', this.onDeviceReady, false);
+
+        // wire up off-canvas menu
+        $('#toggle-offcanvas').bind('tap', function () {
+          $('.row-offcanvas').toggleClass('active');
+        });
+
+        // wire up swipes on the top navbar
+        $('#navbar-top').bind('swipe', function (e) {
+          var hdir = app.getSwipeDirection(e).horizontal;
+          if (hdir === 'left') {
+            $('.row-offcanvas').removeClass('active');
+          } else if(hdir === 'right') {
+            $('.row-offcanvas').addClass('active');
+          }
+        });
+
+        // handle all links
+        $('a').bind('tap', function(e) {
+          app.route(e);
+        });
     },
     // deviceready Event Handler
     //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // The scope of 'this' is the event. In order to call class methods, we
+    // must explicitly call 'app.myMethod(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        // TODO
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    /**
+     * Depends on jQuery mobile's swipe event. Presently reports on horizontal
+     * swipes only; app.getSwipeDirection.direction.vertical will always be null.
+     *
+     * @param {Swipe event} swipe
+     * @returns {object} with properties horizontal ('left', 'right', or null)
+     *                   and vertical (null)
+     */
+    getSwipeDirection: function(swipe) {
+      var startX = swipe.swipestart.coords[0];
+      var endX = swipe.swipestop.coords[0];
+      var direction = {horizontal: null, vertical: null};
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+      if (startX > endX) {
+        direction.horizontal = 'left';
+      } else if(endX > startX) {
+        direction.horizontal = 'right';
+      }
 
-        console.log('Received Event: ' + id);
+      return direction;
+    },
+    /**
+     * Determines how to route requests. Very basic at present. As we start incorporating
+     * user-generated content, we may need additional processing to handle different
+     * types of requests or to handle invalid requests. Possibly this could be handled
+     * by returning app.updateDisplay()'s promise and doing damage control if the
+     * AJAX call fails.
+     *
+     * @param {Tap event} e
+     * @returns {undefined}
+     */
+    route: function(e) {
+      var el = $(e.currentTarget);
+      var destination = el.attr('href');
+
+      // don't try to route external links (i.e., those with a protocol ://); let them behave normally
+      if (!/:\/\//.test(destination)) {
+        e.preventDefault();
+        // TODO: we may need to do more URL processing at a later time, but for now we can pass through
+        app.updateDisplay(destination);
+      }
+    },
+    /**
+     * Updates content on the page and toggles active states for nav links. This
+     * helper function is called from app.route() and should not be called directly.
+     *
+     * @param {string} destination
+     * @returns {undefined}
+     */
+    updateDisplay: function(destination) {
+      // get the path minus any parameters
+      var q = destination.indexOf('?');
+      var basePathLength = (q === -1 ? destination.length : q);
+      var basePath = destination.substr(0, basePathLength);
+
+      $('#content').load(destination, function() {
+        $('.row-offcanvas').removeClass('active'); // any time a link is followed, close the off-canvas menu
+        $('nav a').removeClass('active');
+        $('nav a[href^="' + basePath + '"]').addClass('active');
+        $('#pagetitle').text($('#content title').text());
+      });
     }
 };
 
 $(document).ready(function () {
-  $('#toggle-offcanvas').bind('tap', function () {
-    $('.row-offcanvas').toggleClass('active');
-  });
-  $('#navbar-top').bind('swipe', function (e) {
-    var startX = e.swipestart.coords[0];
-    var endX = e.swipestop.coords[0];
-
-    if (startX > endX) { // swipe left
-      $('.row-offcanvas').removeClass('active');
-    } else if(endX > startX) { // swipe right
-      $('.row-offcanvas').addClass('active');
-    }
-  });
-
-  // load new content when a navigation item is clicked
-  $('[data-destination]').bind('tap', function() {
-    var dest = $(this).data('destination');
-    $('#content').load('pages/' + dest + '.html', function() {
-      $('[data-destination]').removeClass('active');
-      $('[data-destination="' + dest + '"]').addClass('active');
-      $('#pagetitle').text($('#content title').text());
-    });
-  });
+  app.initialize();
 });
