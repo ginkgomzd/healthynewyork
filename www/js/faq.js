2d9595c198bcb7a52cc81c663e64be4bbf2adbe2
ginkgoMZD <mzd@ginkgostreet.com>
2014-10-22 21:23:06 -0400

    troubleshoot in progress loading from SQLite

--- www/js/faq.js
+++ www/js/faq.js
@@ -3,6 +3,7 @@ var faq = _.extend(new Controller(), {
 // Application Constructor
   initialize: function() {
       this.bindEvents();
+
   },
   // Bind Event Listeners
   //
@@ -32,14 +33,50 @@ var faq = _.extend(new Controller(), {
 //      reader.readAsText(file);
 //    }
   main: function() {
-    var tpl_src = $('#faq_tpl').html();
-    var data = {my_var: "my_value"};
-    var template = _.template(tpl_src);
-    this.rendered = template(data);
-
+    this.fetchData();
+    this.bindData(this.data);
     this.updateDisplay();
-  }
+  },
+  fetchData: function() {
+    console.log("GSL:: fetchData");
+    this.data = {};
+    this.data.rows = [];
+    this.data.page_title = 'Terms to Know';
 
+    localDB.db.transaction(this.buildQueries, function(tx,er){
+      console.log("GSL:: transaction ERROR"+ er.message);
+    }, function(a){
+      console.log("GSL:: transaction SUCCESS");
+    });
+  },
+  bindData: function(data) {
+console.log("GSL:: bindData");
+    var src = $('#faq_table_row_tpl').html();
+    var row_tpl = _.template(src);
+    this.data.tbody = '';
+    $.each(this.data.rows, function() {
+      this.data.tbody += row_tpl(this);
+    });
+
+    var tpl_src = $('#table_page_tpl').html();
+    var template = _.template(tpl_src);
+    this.rendered = template(this.data);
+  },
+  buildQueries: function(tx) {
+    console.log("GSL:: buildQueries");
+    $.each(tx, function(key, val) {console.log(key);console.log('][');console.log(val)});
+    tx.executeSql('SELECT "title" FROM "content"', [], this.buildRows,
+    function(tx,er){
+      console.log("GSL:: executeSql ERROR"+ er.message);
+    });
+    console.log("GSL:: didn't die");
+  },
+  buildRows: function(tx, result) {
+    console.log("GSL:: buildRows");
+    for( var i=0; i < result.rows.length; i++ ) {
+      this.data.rows.push(result.rows.item(i).title);
+    }
+  }
 });
 
 $(document).ready(function () {
