b9347dcc287557eb246950335377af84ea1e3359
Frank J. Gómez <frank@ginkgostreet.com>
2014-10-22 22:53:06 -0400

    Got faq controller to a working state. Added some "TODO" code comments.

--- www/js/faq.js
+++ www/js/faq.js
@@ -1,20 +1,16 @@
+// TODO: Do we really need faq.initialize, faq.bindEvents, or faq.onDeviceReady?
+// Do we really need the $(document).ready at the bottom of this file?
 
 var faq = _.extend(new Controller(), {
 // Application Constructor
   initialize: function() {
       this.bindEvents();
-
   },
-  // Bind Event Listeners
-  //
-  // Bind any events that are required on startup. Common events are:
-  // 'load', 'deviceready', 'offline', and 'online'.
   bindEvents: function() {
       // wait for device API libraries to load
       document.addEventListener('deviceready', this.onDeviceReady, false);
   },
-  // The scope of 'this' is the event. In order to call class methods, we
-  // must explicitly call 'bookmark.myMethod(...);'
+  // The scope of 'this' is the event.
   onDeviceReady: function() {
 //      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, faq.gotFS, fail);
   },
@@ -33,29 +29,36 @@ var faq = _.extend(new Controller(), {
 //      reader.readAsText(file);
 //    }
   main: function() {
+    this.content_type = 'Terms to Know';
     this.fetchData();
-    this.bindData(this.data);
-    this.updateDisplay();
   },
+  // TODO: we can probably parameterize this to some extent; if this function could
+  // accept type and ID args, we could build probably all the FAQ pages we need...
+  // at least for lists and individual pages -- for fetching a list of categories
+  // we might want a different function
   fetchData: function() {
-    console.log("GSL:: fetchData");
     this.data = {};
     this.data.rows = [];
-    this.data.page_title = 'Terms to Know';
+    this.data.page_title = this.content_type;
 
-    localDB.db.transaction(this.buildQueries, function(tx,er){
-      console.log("GSL:: transaction ERROR"+ er.message);
-    }, function(a){
-      console.log("GSL:: transaction SUCCESS");
-    });
+    localDB.db.transaction(this.buildQueries,
+      // TODO: we need a generic error handler
+      function(tx, er){
+        console.log("Transaction ERROR: "+ er.message);
+      },
+      function(){
+        faq.bindData();
+        faq.updateDisplay();
+      }
+    );
   },
   bindData: function(data) {
-console.log("GSL:: bindData");
     var src = $('#faq_table_row_tpl').html();
     var row_tpl = _.template(src);
     this.data.tbody = '';
+
     $.each(this.data.rows, function() {
-      this.data.tbody += row_tpl(this);
+      faq.data.tbody += row_tpl(this);
     });
 
     var tpl_src = $('#table_page_tpl').html();
@@ -63,18 +66,16 @@ console.log("GSL:: bindData");
     this.rendered = template(this.data);
   },
   buildQueries: function(tx) {
-    console.log("GSL:: buildQueries");
-    $.each(tx, function(key, val) {console.log(key);console.log('][');console.log(val)});
-    tx.executeSql('SELECT "title" FROM "content"', [], this.buildRows,
-    function(tx,er){
-      console.log("GSL:: executeSql ERROR"+ er.message);
-    });
-    console.log("GSL:: didn't die");
+    tx.executeSql('SELECT "import_id", "title" FROM "content" WHERE type = ?',
+      [faq.content_type],
+      faq.buildRows
+    );
   },
   buildRows: function(tx, result) {
-    console.log("GSL:: buildRows");
-    for( var i=0; i < result.rows.length; i++ ) {
-      this.data.rows.push(result.rows.item(i).title);
+    for(var i=0; i < result.rows.length; i++) {
+      var item = result.rows.item(i);
+      // TODO: this link_url is totally fake
+      faq.data.rows.push({title_text: item.title, link_url: item.import_id});
     }
   }
 });
