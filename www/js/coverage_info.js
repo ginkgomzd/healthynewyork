8c3435bbd3ccd9f01e05fa3b3cc880bc4a9efb4b
Frank J. Gómez <frank@ginkgostreet.com>
2014-10-29 14:10:05 -0400

    First pass at styling coverage info landing page.

--- www/js/coverage_info.js
+++ www/js/coverage_info.js
@@ -7,10 +7,10 @@ var coverage_info = _.extend(new Controller(), {
   fetchData: function() {
     this.data = {};
     this.data.rows = [
-      {controller: 'health_checklist', title: 'Health Checklist'},
-      {controller: 'insurance_basics', title: 'Insurance Basics'},
-      {controller: 'money_saving_tips', title: 'Money-Saving Tips'},
-      {controller: 'health_care_rights', title: 'Health Care Rights'}
+      {content_type: 'health_checklist', title: 'Health Checklist'},
+      {content_type: 'insurance_basics', title: 'Insurance Basics'},
+      {content_type: 'money_saving_tips', title: 'Money-Saving Tips'},
+      {content_type: 'health_care_rights', title: 'Health Care Rights'}
     ];
   },
   render: function(data) {
@@ -20,12 +20,30 @@ var coverage_info = _.extend(new Controller(), {
 
     $.each(this.data.rows, function() {
       coverage_info.rendered += row_tpl({
-        container_class: this.controller,
-        link_url: this.controller,
+        container_class: this.content_type,
+        link_url: 'faq?content_type=' + this.content_type,
         link_text: this.title
       });
     });
 
     this.updateDisplay();
+  },
+  /**
+   * Overrides "parent" method.
+   *
+   * @returns {String} One or more space-separated classes to be added to the class
+   * attribute of the content element
+   */
+  setContentClasses: function() {
+    return 'coverage_info';
+  },
+
+  /**
+   * Overrides "parent" stub method
+   */
+  postUpdateDisplay: function() {
+    var h = 100 / this.data.rows.length;
+    h = h.toString() + '%';
+    $('#content.coverage_info .container-fluid').height(h);
   }
 });
