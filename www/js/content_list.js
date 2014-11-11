ecd5a392be8776a8365614a1a6d26343ca024450
Frank J. Gómez <frank@ginkgostreet.com>
2014-11-11 18:21:42 -0500

    YINY-48: Completed first pass at layout for Health Checklist.

--- www/js/content_list.js
+++ www/js/content_list.js
@@ -24,7 +24,40 @@ var content_list = _.extend(new Controller(), {
     content_list.data.tbody = '';
 
     if (content_list.qs.content_type === 'health_checklist') {
-      // TODO
+      var hc_src = $('#health_checklist_table_row_tpl').html();
+      var hc_row_tpl = _.template(hc_src);
+
+      // we're going to process these two at a time to make a two-column table like so:
+      // 1   3
+      // 2   4
+      var cnt = content_list.data.rows.length;
+      var cnt_half = Math.floor(cnt/2);
+      if (cnt % 2 !== 0) {
+        cnt_half++;
+      }
+      for (var i = 0; i < cnt/2; i++) {
+        var hc_row_data = {};
+        hc_row_data.icon_inner = i + 1;
+        hc_row_data.icon_inner_2 = cnt_half + hc_row_data.icon_inner;
+
+        hc_row_data.icon_class = content_list.data.rows[i].icon_class;
+        hc_row_data.link_url = content_list.data.rows[i].link_url;
+        hc_row_data.title_text = content_list.data.rows[i].title_text;
+
+        var i_2 = cnt_half + i;
+        if (i_2 >= cnt) { // handles the last table cells for an odd number of results
+          hc_row_data.icon_inner_2 = '';
+          hc_row_data.icon_class_2 = '';
+          hc_row_data.link_url_2 = '';
+          hc_row_data.title_text_2 = '';
+        } else {
+          hc_row_data.icon_class_2 = content_list.data.rows[i_2].icon_class;
+          hc_row_data.link_url_2 = content_list.data.rows[i_2].link_url;
+          hc_row_data.title_text_2 = content_list.data.rows[i_2].title_text;
+        }
+
+        content_list.data.tbody += hc_row_tpl(hc_row_data);
+      }
     } else {
       var src = $('#content_list_table_row_tpl').html();
       var row_tpl = _.template(src);
