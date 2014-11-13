79db3042fb84fc1616d6b5b7eff7e1aa3e813cc7
ginkgoMZD <mzd@ginkgostreet.com>
2014-11-13 17:38:39 -0500

    create a zocdoc iframe that is loaded dynamically; proof of concept;

--- www/js/schedule.js
+++ www/js/schedule.js
@@ -3,9 +3,14 @@ var schedule = _.extend(new Controller(), {
     this.renderTpl();
   },
   renderTpl: function() {
+
     var src = $('#schedule_tpl').html();
     var content_tpl = _.template(src);
     this.rendered = content_tpl();
     this.updateDisplay();
+
+    qs='dr_specialty=&address=&insurance_carrier=400&refine_search=Find+a+Doctor';
+    $('#zocdoc_frame').attr('src','http://www.zocdoc.com/'+'?'+ qs);
+
   }
 });
