d02371cf29abab5e774c964fc2aa7d547d2e98cb
ginkgoMZD <mzd@ginkgostreet.com>
2015-07-08 19:03:03 -0400

    un-do troubleshooting comment

--- www/js/app.js
+++ www/js/app.js
@@ -57,14 +57,14 @@ var app = {
         // to some extent is the fact if we do fall prey to a race condition, the app
         // won't crash. Instead the URL will be opened by the app webview rather
         // than the system browser, which isn't so bad.
-//        $(document).on('click', 'a', function(e) {
-//          var el = $(this);
-//          var url = el.attr('href');
-//          if (url && (url.indexOf('://') !== -1)) {
-//            e.preventDefault();
-//            window.open(url, '_system');
-//          }
-//        });
+        $(document).on('click', 'a', function(e) {
+          var el = $(this);
+          var url = el.attr('href');
+          if (url && (url.indexOf('://') !== -1)) {
+            e.preventDefault();
+            window.open(url, '_system');
+          }
+        });
 
         // wire up off-canvas menu
         $('#toggle-offcanvas').bind('click', function () {
